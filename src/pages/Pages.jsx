486ec673fe1e61297d071421fc3a8b09f292b209
VietDatLe <levietdat22@gmail.com>
2023-05-07 22:35:52 +0700

    no message

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import Cuisine from './Cuisine'
-import Home from './Home'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Searched from './Searched'
-import NotFound from './NotFound'
-import Recipe from './Recipe'
-import { AnimatePresence } from 'framer-motion'
+import React from "react";
+import Cuisine from "./Cuisine";
+import Home from "./Home";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Searched from "./Searched";
+import NotFound from "./NotFound";
+import Recipe from "./Recipe";
+import { AnimatePresence } from "framer-motion";
+import PrintableComponent from "../components/RenderPdf";
 //ko xài và không được cả import neu không xài
 function Pages() {
   const location = useLocation();
   return (
-    <AnimatePresence mode='wait' >
+    <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/cuisine/:type' element={<Cuisine></Cuisine>}></Route>
-        <Route path='/searched/:search' element={<Searched></Searched>}></Route>
-        <Route path='*' element={<NotFound />} />
-        <Route path='/recipe/:name' element={<Recipe />} />
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/cuisine/:type" element={<Cuisine></Cuisine>}></Route>
+        <Route path="/searched/:search" element={<Searched></Searched>}></Route>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/recipe/:name" element={<Recipe />} />
+        <Route path="/pdf" element={<PrintableComponent />} />
       </Routes>
     </AnimatePresence>
-
-
-  )
+  );
 }
 
-export default Pages
+export default Pages;
