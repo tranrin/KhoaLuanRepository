f57ac10cde74f8e2284d4ca89c57f64517fcbe37
VIET DAT <levietdat22@gmail.com>
2023-05-07 16:01:25 +0700

    Revert "Revert "Add layout""
    This reverts commit f5de52f77327ffd5e1b662469fb7d4f2a3ddc9f8.

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import Cuisine from './Cuisine'
-import Home from './Home'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Searched from './Searched'
-import NotFound from './NotFound'
-import Recipe from './Recipe'
-import { AnimatePresence } from 'framer-motion'
+import React from "react";
+import Cuisine from "./Cuisine";
+import Home from "./Home";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Searched from "./Searched";
+import NotFound from "./NotFound";
+import Recipe from "./Recipe";
+import { AnimatePresence } from "framer-motion";
+import Profile from "./Profile";
+import CreateRecipe from "./CreateRecipe";
 //ko xài và không được cả import neu không xài
 function Pages() {
   const location = useLocation();
   return (
-    <AnimatePresence mode='wait' >
+    <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/cuisine/:type' element={<Cuisine></Cuisine>}></Route>
-        <Route path='/searched/:search' element={<Searched></Searched>}></Route>
-        <Route path='*' element={<NotFound />} />
-        <Route path='/recipe/:name' element={<Recipe />} />
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/cuisine/:type" element={<Cuisine></Cuisine>}></Route>
+        <Route path="/searched/:search" element={<Searched></Searched>}></Route>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/recipe/:name" element={<Recipe />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/recipe/create" element={<CreateRecipe />} />
       </Routes>
     </AnimatePresence>
-
-
-  )
+  );
 }
 
-export default Pages
+export default Pages;
